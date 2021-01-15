ba013d13882b45d3a4b78980f90087736f8b100e
Borachin <chinbora92@naver.com>
2021-01-15 23:49:16 +0900

    get ready to router

--- src/App.js
+++ src/App.js
@@ -1,65 +1,7 @@
 import React from "react";
-import axios from "axios";
-import Movie from "./Movie";
-import "./App.css";
 
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: []
-  };
-  // state의 초기값을 정의 
-  
-
-  // commit test 
-  // commit test 
-  // commit test 
-  // commit test 
-  // commit test 
-  // commit test 
-  // commit test 
-  // commit test 
-  // commit test 
-
-
-  getMovies = async () => {
-    const {
-      data: {
-        data: {movies}
-      }
-    } = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating"); 
-    this.setState({movies, isLoading: false})
-  };
-  componentDidMount() {
-   this.getMovies();
-  }
-  render() { 
-    // App.js에서 최종적으로 보여지는 render값 정의
-    const { isLoading, movies } = this.state;
-    return (
-      <section className="container">
-        {isLoading ? ( 
-          <div className="loader">
-            <span className="loder__text">Loading..</span>
-          </div>
-        ) : (
-          <div className="movies">
-            {movies.map(movie => (
-              <Movie 
-                key={movie.id}
-                id={movie.id} 
-                year={movie.year} 
-                title={movie.title} 
-                summary={movie.summary} 
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-              />  
-            ))}             
-          </div>  
-        )}  
-      </section>
-    );
-  }
+function App() {
+  return <span>something</span>  
 }
 
 export default App;
